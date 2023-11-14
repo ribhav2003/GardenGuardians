1614f67cee11996ab7e20547829480536633ea6e
AviralChawla20 <aviralchawla02@gmail.com.com>
2023-11-14 09:54:20 +0530

    registerUser.js now automatically creates database and table if not present

--- registerUser.js
+++ registerUser.js
@@ -16,88 +16,111 @@ const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "root", // Replace with your MySQL password
-  database: "plant_care",
 });
 
-connection.connect((err) => {
+// Create the "plant_care" database if it doesn't exist
+connection.query("CREATE DATABASE IF NOT EXISTS plant_care", (err) => {
   if (err) {
-    console.error("Error connecting to MySQL:", err);
+    console.error("Error creating database:", err);
+    connection.end();
     return;
   }
-  console.log("Connected to MySQL");
-});
 
-// API endpoint for user registration
-// Inside the registration endpoint in registerUser.js
-// API endpoint for user registration
-app.post("/register", async (req, res) => {
-  const { username, email, password } = req.body;
+  console.log("Database 'plant_care' is ready");
 
-  // Check if username already exists
-  const usernameCheck = await usernameExists(username);
-  if (usernameCheck) {
-    return res.status(400).json({ error: "Username already exists" });
-  }
-
-  // Check if email already exists
-  const emailCheck = await emailExists(email);
-  if (emailCheck) {
-    return res.status(400).json({ error: "Email already exists" });
-  }
-
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  // Insert user into the database
-  const sql = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
-  connection.query(sql, [username, email, hashedPassword], (err, results) => {
+  // Connect to the "plant_care" database
+  connection.changeUser({ database: "plant_care" }, (err) => {
     if (err) {
-      console.error("Error inserting user:", err);
-      return res.status(500).json({ error: "Internal Server Error" });
+      console.error("Error connecting to database:", err);
+      connection.end();
+      return;
     }
 
-    console.log("User registered successfully");
-    return res.status(200).json({ message: "User registered successfully" });
-  });
-});
+    // Create the "users" table if it doesn't exist
+    connection.query(
+      "CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255), email VARCHAR(255), password VARCHAR(255))",
+      (err) => {
+        if (err) {
+          console.error("Error creating users table:", err);
+          connection.end();
+          return;
+        }
 
-// Helper function to check if a username exists
-function usernameExists(username) {
-  return new Promise((resolve, reject) => {
-    const sql = "SELECT * FROM users WHERE username = ?";
-    connection.query(sql, [username], (err, results) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(results.length > 0);
+        console.log("Table 'users' is ready");
+
+        // Continue with the rest of your code here
+
+        // API endpoint for user registration
+        app.post("/register", async (req, res) => {
+          const { username, email, password } = req.body;
+
+          // Check if username already exists
+          const usernameCheck = await usernameExists(username);
+          if (usernameCheck) {
+            return res.status(400).json({ error: "Username already exists" });
+          }
+
+          // Check if email already exists
+          const emailCheck = await emailExists(email);
+          if (emailCheck) {
+            return res.status(400).json({ error: "Email already exists" });
+          }
+
+          const hashedPassword = await bcrypt.hash(password, 10);
+
+          // Insert user into the database
+          const sql =
+            "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+          connection.query(
+            sql,
+            [username, email, hashedPassword],
+            (err, results) => {
+              if (err) {
+                console.error("Error inserting user:", err);
+                return res.status(500).json({ error: "Internal Server Error" });
+              }
+
+              console.log("User registered successfully");
+              return res
+                .status(200)
+                .json({ message: "User registered successfully" });
+            }
+          );
+        });
+
+        // Helper function to check if a username exists
+        function usernameExists(username) {
+          return new Promise((resolve, reject) => {
+            const sql = "SELECT * FROM users WHERE username = ?";
+            connection.query(sql, [username], (err, results) => {
+              if (err) {
+                reject(err);
+              } else {
+                resolve(results.length > 0);
+              }
+            });
+          });
+        }
+
+        // Helper function to check if an email exists
+        function emailExists(email) {
+          return new Promise((resolve, reject) => {
+            const sql = "SELECT * FROM users WHERE email = ?";
+            connection.query(sql, [email], (err, results) => {
+              if (err) {
+                reject(err);
+              } else {
+                resolve(results.length > 0);
+              }
+            });
+          });
+        }
+
+        // Start the server
+        app.listen(port, () => {
+          console.log(`Server is running on http://localhost:${port}`);
+        });
       }
-    });
+    );
   });
-}
-
-// Helper function to check if an email exists
-function emailExists(email) {
-  return new Promise((resolve, reject) => {
-    const sql = "SELECT * FROM users WHERE email = ?";
-    connection.query(sql, [email], (err, results) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(results.length > 0);
-      }
-    });
-  });
-}
-
-// API endpoint for user login
-// ... (previous code)
-
-// API endpoint for user login
-
-// ... (other code)
-
-// ... (rest of the code)
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
 });
