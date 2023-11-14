5fa14389daa2ab8469ef755aeb6157aa199c73cd
AviralChawla20 <aviralchawla02@gmail.com.com>
2023-11-14 13:58:39 +0530

    created a procedure

--- plant.js
+++ plant.js
@@ -47,6 +47,7 @@ app.get("/searchPlants", async (req, res) => {
   }
 });
 
+// API endpoint for plant details with setup logic
 app.get("/getPlantDetails", async (req, res) => {
   const plantId = req.query.plantId;
 
@@ -55,7 +56,20 @@ app.get("/getPlantDetails", async (req, res) => {
   }
 
   try {
-    // Assuming you have a function to get plant details by ID
+    // Check if the stored procedure exists
+    const checkProcedureQuery =
+      "SHOW PROCEDURE STATUS LIKE 'GetPlantDetailsById';";
+    const [procedureRows] = await connection
+      .promise()
+      .query(checkProcedureQuery);
+    // const procedureExists = await checkIfProcedureExists("GetPlantDetailsById");
+
+    if (procedureRows.length === 0) {
+      // Create the stored procedure
+      await createGetPlantDetailsProcedure();
+    }
+
+    // Call the stored procedure to get plant details
     const plantDetails = await getPlantDetailsById(plantId);
 
     if (!plantDetails) {
@@ -70,26 +84,82 @@ app.get("/getPlantDetails", async (req, res) => {
   }
 });
 
-// Function to get plant details by ID
-// Function to get plant details by ID
+// Function to check if a stored procedure exists
+// Function to check if a stored procedure exists
+async function checkIfProcedureExists(procedureName) {
+  try {
+    const query = `
+      SELECT
+        COUNT(*)
+      FROM
+        information_schema.ROUTINES
+      WHERE
+        ROUTINE_TYPE = 'PROCEDURE'
+        AND ROUTINE_SCHEMA = 'plant_care'
+        AND ROUTINE_NAME = ?;
+    `;
+
+    const [rows] = await connection.promise().query(query, [procedureName]);
+
+    if (rows.length > 0 && rows[0][0] > 0) {
+      return true; // Procedure exists
+    } else {
+      return false; // Procedure does not exist
+    }
+  } catch (error) {
+    console.error("Error checking if procedure exists:", error);
+    throw error;
+  }
+}
+
+// Function to create the stored procedure to get plant details by ID
+// Function to create the stored procedure to get plant details by ID
+async function createGetPlantDetailsProcedure() {
+  try {
+    const getPlantDetailsProcedure = `
+      CREATE PROCEDURE GetPlantDetailsById(IN p_plantId INT)
+      BEGIN
+          SELECT
+              p_id,
+              common_name,
+              scientific_name,
+              cycle,
+              watering,
+              sunlight,
+              image_url
+          FROM
+              plants
+          WHERE
+              p_id = p_plantId;
+      END;
+    `;
+
+    // Execute the procedure creation query
+    await connection.promise().query(getPlantDetailsProcedure);
+  } catch (error) {
+    console.error("Error creating stored procedure:", error);
+    throw error;
+  }
+}
+
+// Function to get plant details by ID using the stored procedure
 async function getPlantDetailsById(plantId) {
-  // Assuming you have a function to execute a query
-  const query = "SELECT * FROM plants WHERE p_id = ?";
+  const query = "CALL GetPlantDetailsById(?)";
   const params = [plantId];
 
   try {
-    const [rows] = await connection.promise().execute(query, params);
+    const [rows] = await connection.promise().query(query, params);
 
-    if (rows && rows.length > 0) {
+    if (rows.length > 0) {
       // Modify the result to include the image URL
       const plantDetails = {
-        p_id: rows[0].p_id,
-        common_name: rows[0].common_name,
-        scientific_name: rows[0].scientific_name,
-        cycle: rows[0].cycle,
-        watering: rows[0].watering,
-        sunlight: rows[0].sunlight,
-        image_url: rows[0].image_url, // Include image URL
+        p_id: rows[0][0].p_id,
+        common_name: rows[0][0].common_name,
+        scientific_name: rows[0][0].scientific_name,
+        cycle: rows[0][0].cycle,
+        watering: rows[0][0].watering,
+        sunlight: rows[0][0].sunlight,
+        image_url: rows[0][0].image_url, // Include image URL
       };
 
       return plantDetails;
@@ -98,11 +168,6 @@ async function getPlantDetailsById(plantId) {
     }
   } catch (error) {
     console.error("Error executing query:", error);
-
-    // Log additional information
-    console.log("Query:", query);
-    console.log("Params:", params);
-
     throw error;
   }
 }
